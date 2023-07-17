f7ada7723930d6e944cffa836969386099adff9f
Julian Salvador <tech.julian.salvador@gmail.com>
2023-07-17 20:24:20 +0800

    Finished the project

--- scripts/app.js
+++ scripts/app.js
@@ -37,3 +37,79 @@ console.log(logoPaths);
 for (let j = 0; j < logoPaths.length; j++) {
   console.log(`Line of area ${j + 1} is ${logoPaths[j].getTotalLength()}`);
 }
+
+//clear fields upon refresh of page
+window.onload = function () {
+  // Reset input fields
+  var inputFields = document.querySelectorAll("input");
+  for (var i = 0; i < inputFields.length; i++) {
+    inputFields[i].value = "";
+  }
+};
+
+const fName = document.querySelector("#firstName");
+const lName = document.querySelector("#lastName");
+const email = document.querySelector("#email");
+const contactNo = document.querySelector("#contactNum");
+const pass = document.querySelector("#password");
+const confirmPass = document.querySelector("#passwordConfirmation");
+
+const errorPopovers = document.querySelectorAll(".error-popover");
+const regForm = document.querySelector("#registerForm");
+const inputFields = document.querySelectorAll("#registerForm fieldset input");
+const formSubmit = document.querySelector("#submitBtn");
+
+inputFields.forEach(function (input) {
+  input.required = true;
+  input.setAttribute("autocomplete", "off");
+
+  input.addEventListener("input", function (event) {
+    const popover = input.parentElement.querySelector(".error-popover");
+    const inputLabel = input.nextElementSibling;
+    if (input.validity.typeMismatch) {
+      popover.style.display = "flex";
+      inputLabel.classList.add("label-levitate");
+      formSubmit.disabled = true;
+    } else {
+      popover.style.display = "none";
+      inputLabel.classList.remove("label-levitate");
+      formSubmit.disabled = false;
+    }
+  });
+});
+
+confirmPass.addEventListener("input", function (event) {
+  const popover = confirmPass.parentElement.querySelector(".error-popover");
+  if (confirmPass.value !== pass.value) {
+    popover.style.display = "flex";
+    formSubmit.disabled = true;
+  } else {
+    popover.style.display = "none";
+    formSubmit.disabled = false;
+  }
+});
+
+// const inputLabel = input.nextElementSibling;
+
+// input.addEventListener("focus", function () {
+//   inputLabel.classList.add("label-levitate");
+// });
+
+// input.addEventListener("blur", function () {
+//   inputLabel.classList.remove("label-levitate");
+// });
+
+//Limiting names
+// Get all input fields with data-type="name"
+const nameInputs = document.querySelectorAll('input[data-type="name"]');
+
+// Iterate over the input fields
+nameInputs.forEach(function (input) {
+  // Add event listener for input event
+  input.addEventListener("input", function () {
+    // Remove any non-letter characters from the input value
+    var filteredValue = this.value.replace(/[^A-Za-z]/g, "");
+    // Update the input value with the filtered value
+    this.value = filteredValue;
+  });
+});
